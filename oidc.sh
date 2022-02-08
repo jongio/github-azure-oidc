d733be7c811cb8fef110e0331a246fc5c654a231
Jon Gallant <jon@jongallant.com>
2022-02-08 15:58:04 -0800

    Add FIC_EXISTS check after creating FIC

--- oidc.sh
+++ oidc.sh
@@ -8,7 +8,8 @@ set -euo pipefail
 # ./oidc.sh {APP_NAME} {ORG|USER/REPO} {FICS_FILE}
 # ./oidc.sh ghazoidc1 jongio/ghazoidctest ./fics.json
 IS_CODESPACE=${CODESPACES:-"false"}
-if $IS_CODESPACE == "true"; then
+if $IS_CODESPACE == "true"
+then
     echo "This script doesn't work in GitHub Codespaces.  See this issue for updates. https://github.com/Azure/login/issues/177"
     exit 0
 fi
@@ -20,7 +21,8 @@ FICS_FILE=$3
 echo "Checking Azure CLI login status..."
 EXPIRED_TOKEN=$(az ad signed-in-user show --query 'objectId' -o tsv || true)
 
-if [[ -z "$EXPIRED_TOKEN" ]]; then
+if [[ -z "$EXPIRED_TOKEN" ]]
+then
     az login -o none
 fi
 
@@ -54,7 +56,8 @@ echo "Configuring application..."
 #  First check if an app with the same name exists, if so use it, if not create one
 APP_ID=$(az ad app list --filter "displayName eq '$APP_NAME'" --query [].appId -o tsv)
 
-if [[ -z "$APP_ID" ]]; then
+if [[ -z "$APP_ID" ]]
+then
     echo "Creating AD app..."
     APP_ID=$(az ad app create --display-name ${APP_NAME} --query appId -o tsv)
     echo "Sleeping for 30 seconds to give time for the APP to be created."
@@ -69,7 +72,8 @@ echo "Configuring Service Principal..."
 
 echo "First checking if the Service Principal already exists..."
 SP_ID=$(az ad sp list --filter "appId eq '$APP_ID'" --query [].objectId -o tsv)
-if [[ -z "$SP_ID" ]]; then
+if [[ -z "$SP_ID" ]]
+then
     echo "Creating service principal..."
     SP_ID=$(az ad sp create --id $APP_ID --query objectId -o tsv)
 
@@ -88,25 +92,61 @@ echo "SP_ID: $SP_ID"
 APP_OBJECT_ID=$(az ad app show --id $APP_ID --query objectId -o tsv)
 echo "APP_OBJECT_ID: $APP_OBJECT_ID"
 
+
+# Function that accepts a subject and returns 0 if it exists and 1 if it doesn't
+FIC_EXISTS(){
+    local SUBJECT=$1
+    local ALL_FICS=$(az rest --method GET --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials")
+    local SUBJECT_FIC=$(jq -r --arg SUBJECT "$1" '.value[] | select(.subject==$SUBJECT)' <<< "${ALL_FICS}")
+    if [ -z "$SUBJECT_FIC" ]
+    then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+
 echo "Creating federatedIdentityCredentials..."
+echo 
 for FIC in $(envsubst < $FICS_FILE | jq -c '.[]'); do
     SUBJECT=$(jq -r '.subject' <<< "$FIC")
-    ALL_FICS=$(az rest --method GET --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials")
-    SUBJECT_FIC=$(jq -r --arg SUBJECT "$SUBJECT" '.value[] | select(.subject==$SUBJECT)' <<< "${ALL_FICS}")
-    if [ -z "$SUBJECT_FIC" ]; then
-        echo "Creating FIC with subject '${SUBJECT}'."
-        az rest --method POST --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials" --body ${FIC}
-        # Adding a sleep here seems to help the FICs get created.
-        sleep 10s
-    else 
-        echo "FIC with subject '${SUBJECT}' already exists. Skipping..."
+    
+    DOES_FIC_EXIST=$(FIC_EXISTS $SUBJECT)
+
+    if [ $DOES_FIC_EXIST -eq 0 ]
+    then
+        echo "FIC with subject '${SUBJECT}' already exists..."
+        echo
+    else
+        while [ $DOES_FIC_EXIST -eq 1 ]
+        do
+            echo "Creating FIC with subject '${SUBJECT}'."
+            az rest --method POST --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials" --body ${FIC}
+            # Adding a sleep here seems to help the FICs get created.
+            
+            echo "Sleeping for 10s before checking if the newly created FIC exists..."
+            sleep 10s
+
+            # Verify that the FIC was created
+            DOES_FIC_EXIST=$(FIC_EXISTS $SUBJECT)
+            if [ $DOES_FIC_EXIST -eq 0 ]
+            then
+                echo "The FIC was successfully created."
+            else
+                echo "The FIC wasn't created successfully, retrying..."
+            fi
+            echo
+        done
     fi
 done
 
 # To get an Azure AD app FICs
-#az rest --method GET --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials"
+# az rest --method GET --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials"
+
 # To delete an Azure AD app FIC
-#az rest --method DELETE --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials/${FIC_ID}"
+# az rest --method DELETE --uri "https://graph.microsoft.com/beta/applications/${APP_OBJECT_ID}/federatedIdentityCredentials/${FIC_ID}"
+
 # You can also delete FICs here: 
 # https://ms.portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/Credentials/appId/${APP_ID}/isMSAApp/
 
